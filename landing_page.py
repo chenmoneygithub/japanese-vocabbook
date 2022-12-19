edc66499522ad0632d760c5728ea2ae6295f9574
ThomasChen94 <qianchen94era@gmail.com>
2022-12-18 20:07:18 -0800

    some initial commits

--- landing_page.py
+++ landing_page.py
@@ -2,22 +2,26 @@ import csv
 import random
 import re
 
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 def parse_vocab():
-  with open('vocab.csv') as csv_file:
-      csv_reader = csv.reader(csv_file, delimiter=',')
-      line_count = 0
-      res = []
-      for row in csv_reader:
-          elements = re.split(r"\t+", row[0])
-          res.append((elements[0], elements[1]))
-  return res
+    with open('vocab.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        res = []
+        for row in csv_reader:
+            elements = re.split(r"\t+", row[0])
+            res.append((elements[0], elements[1]))
+    return res
 
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    vocabs = parse_vocab()
+    if request.method == 'POST':
+        if request.form.get('action1') == 'VALUE1':
+            return render_template('index.html', vocabs=vocabs)
+    elif request.method == 'GET':
+        return render_template('index.html', vocabs=[])
